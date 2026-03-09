77aba7b358a547c5efaabf644f825c7172ce85e9
Painfu11y <333vahan777@gmail.com>
2026-03-09 22:15:43 +0400

    refactor: repository findByTraineeFilter() method logic realization

--- src/main/java/com/gym_project/repository/impl/TrainingRepositoryImpl.java
+++ src/main/java/com/gym_project/repository/impl/TrainingRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.gym_project.repository.impl;
 
+import com.gym_project.dto.request.TraineeTrainingsFilterRequestDto;
 import com.gym_project.entity.Trainee;
 import com.gym_project.entity.Trainer;
 import com.gym_project.entity.Training;
@@ -9,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -31,11 +34,51 @@ public class TrainingRepositoryImpl implements TrainingRepository {
 
     @Override
     @Transactional(readOnly = true)
-    public List<Training> findByTrainee(Trainee trainee) {
-        return entityManager.createQuery(
-                        "SELECT t FROM Training t WHERE t.trainee = :trainee", Training.class)
-                .setParameter("trainee", trainee)
-                .getResultList();
+    public List<Training> findByTraineeFilter(TraineeTrainingsFilterRequestDto filter) {
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Training> query = cb.createQuery(Training.class);
+
+        Root<Training> training = query.from(Training.class);
+        Join<Object, Object> trainer = training.join("trainer");
+        Join<Object, Object> trainingType = training.join("trainingType");
+        Join<Object, Object> trainee = training.join("trainee");
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        predicates.add(cb.equal(trainee.get("username"), filter.getUsername()));
+
+        if (filter.getPeriodFrom() != null) {
+            predicates.add(cb.greaterThanOrEqualTo(
+                    training.get("trainingDate"),
+                    filter.getPeriodFrom()
+            ));
+        }
+
+        if (filter.getPeriodTo() != null) {
+            predicates.add(cb.lessThanOrEqualTo(
+                    training.get("trainingDate"),
+                    filter.getPeriodTo()
+            ));
+        }
+
+        if (filter.getTrainerName() != null && !filter.getTrainerName().isBlank()) {
+            predicates.add(cb.like(
+                    cb.lower(trainer.get("firstName")),
+                    "%" + filter.getTrainerName().toLowerCase() + "%"
+            ));
+        }
+
+        if (filter.getTrainingType() != null && !filter.getTrainingType().isBlank()) {
+            predicates.add(cb.equal(
+                    trainingType.get("trainingTypeName"),
+                    filter.getTrainingType()
+            ));
+        }
+
+        query.where(predicates.toArray(new Predicate[0]));
+
+        return entityManager.createQuery(query).getResultList();
     }
 
     @Override
