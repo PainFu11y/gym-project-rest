9a5c656e5822ccf40d02b56677c0d9265a2d74a9
Painfu11y <333vahan777@gmail.com>
2026-03-09 22:57:15 +0400

    refactor: findByTrainerFilter improvement

--- src/main/java/com/gym_project/repository/impl/TrainingRepositoryImpl.java
+++ src/main/java/com/gym_project/repository/impl/TrainingRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.gym_project.repository.impl;
 
 import com.gym_project.dto.request.TraineeTrainingsFilterRequestDto;
+import com.gym_project.dto.request.TrainerTrainingsRequestDto;
 import com.gym_project.entity.Trainee;
 import com.gym_project.entity.Trainer;
 import com.gym_project.entity.Training;
@@ -83,10 +84,36 @@ public class TrainingRepositoryImpl implements TrainingRepository {
 
     @Override
     @Transactional(readOnly = true)
-    public List<Training> findByTrainer(Trainer trainer) {
-        return entityManager.createQuery(
-                        "SELECT t FROM Training t WHERE t.trainer = :trainer", Training.class)
-                .setParameter("trainer", trainer)
-                .getResultList();
+    public List<Training> findByTrainerFilter(TrainerTrainingsRequestDto filter) {
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Training> query = cb.createQuery(Training.class);
+        Root<Training> training = query.from(Training.class);
+
+        Join<Training, Trainee> trainee = training.join("trainee", JoinType.INNER);
+        Join<Training, Trainer> trainer = training.join("trainer", JoinType.INNER);
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (filter.getUsername() != null && !filter.getUsername().isBlank()) {
+            predicates.add(cb.equal(trainer.get("username"), filter.getUsername()));
+        }
+
+        if (filter.getPeriodFrom() != null) {
+            predicates.add(cb.greaterThanOrEqualTo(training.get("trainingDate"), filter.getPeriodFrom()));
+        }
+        if (filter.getPeriodTo() != null) {
+            predicates.add(cb.lessThanOrEqualTo(training.get("trainingDate"), filter.getPeriodTo()));
+        }
+
+        if (filter.getTraineeName() != null && !filter.getTraineeName().isBlank()) {
+            Predicate firstNameMatch = cb.like(cb.lower(trainee.get("firstName")), "%" + filter.getTraineeName().toLowerCase() + "%");
+            Predicate lastNameMatch = cb.like(cb.lower(trainee.get("lastName")), "%" + filter.getTraineeName().toLowerCase() + "%");
+            predicates.add(cb.or(firstNameMatch, lastNameMatch));
+        }
+
+        query.where(predicates.toArray(new Predicate[0]));
+
+        return entityManager.createQuery(query).getResultList();
     }
 }
