dac05442ffcab85924fbcea6c2c516ce191a3716
Painfu11y <333vahan777@gmail.com>
2026-03-09 20:15:20 +0400

    feat: ChangePasswordRequestDto implementation

--- src/main/java/com/gym_project/dto/request/ChangePasswordRequestDto.java
+++ src/main/java/com/gym_project/dto/request/ChangePasswordRequestDto.java
@@ -1,4 +1,20 @@
 package com.gym_project.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
 public class ChangePasswordRequestDto {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String oldPassword;
+
+    @NotBlank
+    private String newPassword;
 }
