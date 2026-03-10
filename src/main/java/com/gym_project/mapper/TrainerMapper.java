a558efe3be491631c1ce382a2274019ebc17c4dc
Painfu11y <333vahan777@gmail.com>
2026-03-11 01:48:54 +0400

    feat: trainer mapper function

--- src/main/java/com/gym_project/mapper/TrainerMapper.java
+++ src/main/java/com/gym_project/mapper/TrainerMapper.java
@@ -1,6 +1,7 @@
 package com.gym_project.mapper;
 
 import com.gym_project.dto.common.TraineeSummaryDto;
+import com.gym_project.dto.create.response.TrainerCreateResponseDto;
 import com.gym_project.dto.response.TraineeSummaryResponseDto;
 import com.gym_project.dto.response.TrainerResponseDto;
 import com.gym_project.dto.update.response.TrainerUpdateResponseDto;
@@ -22,9 +23,16 @@ public interface TrainerMapper {
     @Mapping(target = "trainees", source = "trainees", qualifiedByName = "mapTraineeSummary")
     TrainerResponseDto toResponseDto(Trainer trainer);
 
+    List<TrainerResponseDto> toResponseDtoList(List<Trainer> trainers);
+
+    TrainerCreateResponseDto toCreateResponseDto(Trainer trainer);
+
+
     @Mapping(target = "specialization", source = "specialization", qualifiedByName = "mapTrainingType")
     @Mapping(target = "trainees", source = "trainees", qualifiedByName = "mapTraineeSummaryResponse")
     TrainerUpdateResponseDto toUpdateResponseDto(Trainer trainer);
+
+    List<TrainerUpdateResponseDto> toUpdateResponseDtoList(List<Trainer> trainers);
 
     @Named("mapTrainingType")
     default String mapTrainingType(TrainingType type) {
